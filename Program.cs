4f50f5c38d41109943f7e77b015c4f99d9ae9c35
fefeborges <fernandavargas1011@gmail.com>
2023-10-26 10:00:02 -0300

    Alteração string de conexão.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto>
-    (options => options.UseSqlServer("Data Source=SB-1490653\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Fernanda
+    (options => options.UseSqlServer("Data Source=SB-1490653\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
+
+/*builder.Services.AddDbContext<Contexto> //Júlia
+    (options => options.UseSqlServer("Data Source=SB-1490628\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
+
+/*builder.Services.AddDbContext<Contexto> //Caroline
+    (options => options.UseSqlServer("Data Source=SB-1490624\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
+
+/*builder.Services.AddDbContext<Contexto> //Lívia
+    (options => options.UseSqlServer("Data Source=SB-1490639\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
 
 var app = builder.Build();
 
