4d5ea6d49c0300ba3272f0870eca99d32b2ed6f8
juliamenegassi <julia.menegass159@gmail.com>
2023-10-27 13:41:29 -0300

    Correção da fonte

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Fernanda
     (options => options.UseSqlServer("Data Source=SB-1490653\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
 
-/*builder.Services.AddDbContext<Contexto> //Júlia
-    (options => options.UseSqlServer("Data Source=SB-1490628\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
+builder.Services.AddDbContext<Contexto> //Júlia
+    (options => options.UseSqlServer("Data Source=SB-1490628\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));
 
-builder.Services.AddDbContext<Contexto> //Caroline
-    (options => options.UseSqlServer("Data Source=SB-1490624\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Caroline
+    (options => options.UseSqlServer("Data Source=SB-1490624\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
 
 /*builder.Services.AddDbContext<Contexto> //Lívia
     (options => options.UseSqlServer("Data Source=SB-1490639\\SQLSENAI;Initial Catalog = ProjetoAgencia;Integrated Security = True;TrustServerCertificate = True"));*/
